11e4bf999554b91c0b56f553e11573fa8cae7256
paresh27 <kumarpareshmaheshwari@gmail.com>
2023-07-27 17:34:07 +0530

    install validator package from NPM

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,7 @@
 const myNotes = require("./notes.js");
+const validator = require("validator");
 
 console.log(myNotes());
+
+console.log(validator.isEmail("123"));
+console.log(validator.isEmail("test@test.com"));
