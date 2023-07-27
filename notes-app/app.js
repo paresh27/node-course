88663bf59f11388b126d3d423325350c9b6d3808
paresh27 <kumarpareshmaheshwari@gmail.com>
2023-07-27 19:59:21 +0530

    using yargs to create custom commands.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,72 @@
 const myNotes = require("./notes.js");
 const chalk = require("chalk");
+const yargs = require("yargs");
 
-console.log(myNotes());
+// customize yargs version
+yargs.version("1.1.0");
 
-console.log(chalk.green.bold.inverse("Success!!"));
+// creating add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title", argv.title);
+    console.log("Body", argv.body);
+  },
+});
+
+//creating remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title", argv.title);
+  },
+});
+
+//creating list command
+yargs.command({
+  command: "list",
+  describe: "List all notes",
+  handler: function () {
+    console.log("Listing all notes!");
+  },
+});
+
+//creating read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title", argv.title);
+  },
+});
+
+// console.log(yargs.argv);
+yargs.parse();
