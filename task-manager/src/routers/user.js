089a05002772444db76728371997e0d6c0fe4fff
paresh1734 <paresh.m@simformsolutions.com>
2023-08-23 17:30:00 +0530

    added email sending feature.

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -3,12 +3,14 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 const multer = require("multer");
+const { sendWelcomeEmail, sendCancelEmail } = require("../emails/account");
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
     
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name);
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
@@ -52,7 +54,8 @@ router.patch('/users/me', auth, async (req, res) => {
 
 router.delete('/users/me', auth, async (req, res) => {
     try {
-        await req.user.remove()        
+        await req.user.remove()   
+        sendCancelEmail(req.user.email, req.user.name);
         res.send(req.user)
     } catch (e) {
         res.status(500).send()
