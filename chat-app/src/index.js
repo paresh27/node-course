f01fdf0bc0b849fb386d5f6e02b34916e2a722f6
paresh1734 <paresh.m@simformsolutions.com>
2023-08-25 13:38:53 +0530

    setup socket.io  and a basic connection between client and server for counter application

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,6 +1,11 @@
 const path = require("path");
 const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 
@@ -8,6 +13,15 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+let count = 0;
+io.on("connection", (socket) => {
+  socket.emit("countUpdated", count);
+  socket.on("increment", () => {
+    count++;
+    io.emit("countUpdated", count);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Application running on port ${port}`);
 });
