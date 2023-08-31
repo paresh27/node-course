dc4a7e6d8f4ad6e2df2a4232f8088ae85ddd8022
paresh1734 <paresh.m@simformsolutions.com>
2023-08-31 11:04:33 +0530

    added a bad words filter library, and way to send message and share locations.

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,9 +1,61 @@
 const socket = io();
 
-socket.on("countUpdated", (count) => {
-  console.log("The count has been updated!", count);
+//Elements
+const $messageForm = document.querySelector("#chat-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $locationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+const $locations = document.querySelector("#locations");
+
+//templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationTemplate = document.querySelector("#location-template").innerHTML;
+
+socket.on("welcomeMessage", (message) => {
+  const html = Mustache.render(messageTemplate, { message });
+  $messages.insertAdjacentHTML("beforeend", html);
 });
 
-document.querySelector("#increment").addEventListener("click", () => {
-  socket.emit("increment");
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+    if (error) {
+      console.log(error);
+    }
+    console.log("Message was delivered!");
+  });
+});
+
+$locationButton.addEventListener("click", (e) => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser.");
+  }
+  $locationButton.setAttribute("disabled", "disabled");
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        $locationButton.removeAttribute("disabled");
+        console.log("Location shared");
+      }
+    );
+  });
+});
+
+socket.on("sendLocation", (url) => {
+  const html = Mustache.render(locationTemplate, { url });
+  $locations.insertAdjacentHTML("beforeend", html);
 });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app);
@@ -13,12 +14,35 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
+let message = "Welcome!";
 io.on("connection", (socket) => {
-  socket.emit("countUpdated", count);
-  socket.on("increment", () => {
-    count++;
-    io.emit("countUpdated", count);
+  socket.emit("welcomeMessage", message);
+
+  socket.broadcast.emit("welcomeMessage", "A new user has joined!");
+
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed.");
+    }
+
+    io.emit("welcomeMessage", message);
+
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("welcomeMessage", "A user had left!");
+  });
+
+  socket.on("sendLocation", ({ latitude, longitude }, callback) => {
+    // console.log("sendLocation", latitude, longitude);
+    socket.broadcast.emit(
+      "sendLocation",
+      `https://google.com/maps?q=${latitude},${longitude}`
+    );
+    callback();
   });
 });
 
